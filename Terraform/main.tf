4c7b09f9f63e5f20d8d463c11dbb3aac9d09ad7d
celebaldeepak <109505220+celebaldeepak@users.noreply.github.com>
2022-11-29 15:54:26 +0530

    Added Deepak
    added deepak

--- Terraform/main.tf
+++ Terraform/main.tf
@@ -4,7 +4,7 @@ resource "azurerm_resource_group" "example" {
 }
 
 resource "azurerm_app_service_plan" "example" {
-  name                = "example-appserviceplan"
+  name                = "Deepak-example-appserviceplan"
   location            = azurerm_resource_group.example.location
   resource_group_name = azurerm_resource_group.example.name
 
